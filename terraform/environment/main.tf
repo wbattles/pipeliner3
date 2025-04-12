b2fa5001df98fd58d0e7532049d144c5ae2ff30a
wbattles <wileybattles@gmail.com>
2025-04-12 14:19:42 -0500

    update launch templates

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -3,7 +3,7 @@ module "my_ec2" {
 
   instance_name   = var.instance_name
   instance_type   = var.instance_type
-  launch_template_id = var.launch_template_id
+  launch_template_id = var.launch_template_id != "" ? var.launch_template_id : null
   ami_id          = var.ami_id
   vpc_id          = var.vpc_id
   subnet_id       = var.subnet_id
