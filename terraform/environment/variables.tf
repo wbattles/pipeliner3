6e066ab144ef80fe746f7a12a5c3f0f148b2b7c5
Wiley Battles <wileybattles@gmail.com>
2025-04-12 14:29:02 -0500

    fix launch temp default

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -1,6 +1,8 @@
 variable "vpc_id" {}
 variable "subnet_id" {}
-variable "launch_template_id" {}
+variable "launch_template_id" {
+    default = ""
+}
 variable "ami_id" {}
 variable "instance_name" {}
 variable "instance_type" {}
