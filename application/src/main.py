14888fa94ca5a511069b2dc9ea7c73c4c52e0079
Wiley Battles <wileybattles@gmail.com>
2025-04-29 17:35:07 -0500

    add db to app

--- application/src/main.py
+++ application/src/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import boto3
 import logging
 
 logger = logging.getLogger(__name__)
 
-class GetSecretWrapper:
+class SecretWrapper:
     def __init__(self, secretsmanager_client):
         self.client = secretsmanager_client
 
@@ -28,12 +28,44 @@ def get_secret(secret_name, region_name):
         region_name=region_name
     )
 
-    secret = GetSecretWrapper(client)
+    secret = SecretWrapper(client)
     return secret.get_secret(secret_name)
-    
+
+class DynamoDBWrapper:
+    def __init__(self, table):
+        self.table = table
+
+    def get_item(self, key):
+        try:
+            response = self.table.get_item(Key=key)
+            return response.get('Item', None)
+        except Exception as e:
+            logger.error(f"An error occurred while getting item: {str(e)}.")
+            raise
+
+    def put_item(self, name, color, type):
+        try:
+            response = self.table.put_item(
+                Item={
+                    'name': name,
+                    'color': color,
+                    'type': type
+                }
+            )
+            return response
+        except Exception as e:
+            logger.error(f"An error occurred while adding movie: {str(e)}.")
+            raise
+
 def create_app():
     app = Flask(__name__)
     
+    region_name = "us-east-1"
+    table_name = "test-table"
+    dynamodb_resource = boto3.resource('dynamodb', region_name=region_name)
+    table = dynamodb_resource.Table(table_name)
+    db_wrapper = DynamoDBWrapper(table)
+
     @app.route('/')
     def home():
         secret_name = "TestSecret"
@@ -42,4 +74,26 @@ def create_app():
 
         return secret_value
     
+    @app.route('/get_item/<item_id>', methods=['GET'])
+    def get_item(item_id):
+        try:
+            item = db_wrapper.get_item({'itemID': item_id})
+            if item:
+                return item
+            else:
+                return {"error": "Item not found"}, 404
+        except Exception as e:
+            logger.error(f"An error occurred while retrieving item: {str(e)}.")
+            return {"error": str(e)}, 500
+
+    @app.route('/add_item', methods=['POST'])
+    def add_item():
+        try:
+            item_data = request.get_json()
+            response = table.put_item(Item=item_data)
+            return jsonify({"message": "Item added", "response": response}), 201
+        except Exception as e:
+            logger.error(f"Error putting item: {e}")
+            return jsonify({"error": str(e)}), 500
+
     return app
